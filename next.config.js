ce0e0e537d39511fa8e1eff1c2b11d4ba686e783
SyedDaniyalHassan <57272783+SyedDaniyalHassan@users.noreply.github.com>
2024-07-22 18:31:28 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,9 +13,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withSentryConfig(nextConfig, {
-  org: "nixlab-technologies",
-  project: "portfolio-nextjs",
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-  silent: false,
-});
+module.exports = nextConfig
